0bf7319afebc3b96730550084616a8cc303e8563
Anwaripasha Akbar <pashaakbar52@gmail.com>
2023-05-28 21:08:47 +0700

    make keseharianku clickable

--- src/components/porto.jsx
+++ src/components/porto.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import kawakib from '../assets/kawakib.png';
 import { Link } from 'react-scroll'
 import awan from '../assets/awan.png'
+import keseharianku from '../assets/keseharianku.png'
 
 const Porto = () => {
   return (
@@ -10,15 +11,15 @@ const Porto = () => {
         <div class="flex flex-wrap text-center justify-around">
             <div class="max-w-sm rounded cursor-pointer overflow-hidden shadow-lg hover:scale-105 duration-200 rounded-[10px]">
                 <a href="https://main--kawakibpashadev.netlify.app" smooth duration={400}>
-                <img class="w-full" src={kawakib} alt="Sunset in the mountains"/>
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">Kawakib</div>
-                    <p class="text-gray-700 text-base">An astronomy platform for casual enthusiasts is a platform that provides information, knowledge, and fun features related to astronomy. This platform is designed to satisfy the curiosity and interests of casual enthusiasts who are intrigued by the world of astronomy, without requiring in-depth knowledge of the subject.</p>
-                </div>
-                <div class="px-6 pt-4 pb-2">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#astronomy</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#nasa</span>
+                    <img class="w-full" src={kawakib} alt="Sunset in the mountains"/>
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">Kawakib</div>
+                        <p class="text-gray-700 text-base">An astronomy platform for casual enthusiasts is a platform that provides information, knowledge, and fun features related to astronomy. This platform is designed to satisfy the curiosity and interests of casual enthusiasts who are intrigued by the world of astronomy, without requiring in-depth knowledge of the subject.</p>
+                    </div>
+                    <div class="px-6 pt-4 pb-2">
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#astronomy</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#nasa</span>
                 </div>
                 </a>
             </div>
@@ -33,18 +34,21 @@ const Porto = () => {
                     <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#web</span>
                     <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#forecast</span>
                 </div>
+
             </div>
-            <div class="max-w-sm rounded overflow-hidden shadow-lg rounded-[10px]">
-                <img class="w-full" src="https://cdn.catboys.com/images/image_134.jpg" alt="Sunset in the mountains"/>
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-                    <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
-                </div>
-                <div class="px-6 pt-4 pb-2">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#nasa</span>
-                </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg hover:scale-105 duration-200 rounded-[10px]">
+                <a href="https://super-cajeta-dea8b9.netlify.app" smooth duration={400}>
+                    <img class="w-full" src={keseharianku} alt="Sunset in the mountains"/>
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">KeseharianKu</div>
+                        <p class="text-gray-700 text-base">A Simple To Do List Website</p>
+                    </div>
+                    <div class="px-6 pt-4 pb-2">
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#todo</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#task</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#activity</span>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
